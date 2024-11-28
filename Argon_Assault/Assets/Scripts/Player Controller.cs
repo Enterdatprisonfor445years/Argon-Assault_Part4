0e570700b0d17be6ddec0b37a0744ec83d0a409c
Enterdatprisonfor445years <85706754+Enterdatprisonfor445years@users.noreply.github.com>
2024-11-28 18:14:26 -0500

    Added Header and Tooltip attributes for variable organization.
    4.24 Header & Tooltips Attributes

--- Argon_Assault/Assets/Scripts/Player Controller.cs
+++ Argon_Assault/Assets/Scripts/Player Controller.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("General Setup Settings")]
+    [Tooltip("How fast ship moves up and down based upon player input")]
     [SerializeField] float controlSpeed = 10f;
-    [SerializeField] float xRange = 10f;
-    [SerializeField] float yRange = 7f;
+    [Tooltip("How fast player moves horizontally")] [SerializeField] float xRange = 10f;
+    [Tooltip("How fast player moves vertically")] [SerializeField] float yRange = 7f;
+
+    [Header("Laser gun array")]
+    [Tooltip("Add all player lasers here")]
     [SerializeField] GameObject[] lasers;
 
+    [Header("Screen position based tuning")]
     [SerializeField] float positionPitchFactor = -2f;
     [SerializeField] float controlPitchFactor = -10f;
+
+    [Header("Player input based tuning")]
     [SerializeField] float positionYawFactor = 2f;
     [SerializeField] float controlRollFactor = -20f;
 
