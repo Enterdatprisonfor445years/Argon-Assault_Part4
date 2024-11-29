91113c3a7cb4a4bb46e764f0a3504797bf664942
Enterdatprisonfor445years <85706754+Enterdatprisonfor445years@users.noreply.github.com>
2024-11-29 03:56:17 -0500

    Public methods implemented for class communication.
    4.31 Public Methods In Unity C#

--- Argon_Assault/Assets/Scripts/enemy.cs
+++ Argon_Assault/Assets/Scripts/enemy.cs
@@ -6,10 +6,29 @@ public class enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
     void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
     }
 }
